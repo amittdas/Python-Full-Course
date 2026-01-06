49b95f4ad8cf5b8e70ad166ad389ca05092b83a0
amittdas <amitdas123456789@gmail.com>
2026-01-06 21:33:19 +0530

    Add CONTROL FLOW & LOOPS

---
+++ 02_CONTROL FLOW & LOOPS/04_While Loops in python/02_P1.py
@@ -0,0 +1,10 @@
+# Write a program that keeps asking user to enter a password until they enter the correct one.
+
+password = "Amit@123";
+
+passs = input("Enter your password: ")
+
+while( passs != password):
+    passs = input("Enter yor password again: ")
+    
+print("Success")
---
+++ 02_CONTROL FLOW & LOOPS/04_While Loops in python/03_P2.py
@@ -0,0 +1,3 @@
+# Use a while loop to reverse a given number (123  --->   321)
+
+num = 123
