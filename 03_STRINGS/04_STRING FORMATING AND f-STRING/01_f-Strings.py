34ac28e1d168cc9bcd931242b9e09f4c1f6e3044
amittdas <amitdas123456789@gmail.com>
2026-01-11 21:54:53 +0530

    Add Strings

---
+++ 03_STRINGS/04_STRING FORMATING AND f-STRING/01_f-Strings.py
@@ -0,0 +1,15 @@
+template = "Dear {}, You are awesome. Take this {}$ bag"
+
+a = "John"
+a1 = 1000
+
+b = "Jack"
+b1 = 2000
+
+c = "Marie"
+c1 = 3000
+
+s1 = template.format(a,a1)
+print(s1)
+
+print(f"Hey {b}, This is fstring class and I want to tell you that You are awesome and take this {b1} bag")
---
+++ 03_STRINGS/04_STRING FORMATING AND f-STRING/02_p1.py
@@ -0,0 +1,8 @@
+# Using f-string, create a sentence:
+# "My name is john and I am 25 years old"
+# by passing 'john' and '25' as variables.
+
+name = "John"
+age = 25
+
+print(f"My name is {name} and I am {age} years old ")
