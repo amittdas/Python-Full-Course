4cbdebb5dc13cffb0575bad64be0b1d93eebffd2
amittdas <amitdas123456789@gmail.com>
2026-01-16 16:25:03 +0530

    Add RECURSON

---
+++ 04_FUNCTIONS AND MODULE/04_RECURSION/01_fibonacci.py
@@ -0,0 +1,7 @@
+def fib(n):
+    if(n == 0 or n == 1):
+        return n
+    return (fib(n-2) + fib(n-1))
+
+print(fib(5))
+print(fib(6))
---
+++ 04_FUNCTIONS AND MODULE/04_RECURSION/02_factorial.py
@@ -0,0 +1,11 @@
+def fact(n):
+    if(n == 0 or n == 1):
+        return 1
+    return n*fact(n-1)
+
+print(fact(1))
+print(fact(2))
+print(fact(3))
+print(fact(4))
+print(fact(5))
+print(fact(6))
