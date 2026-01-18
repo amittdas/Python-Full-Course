d5272d18ec1dac3262a534cff567c8e5e272f5f1
amittdas <amitdas123456789@gmail.com>
2026-01-19 00:03:57 +0530

    Add LISTS

---
+++ 05_DATA STRUCTURES/01_LISTS & LIST METHODS/01_lists.py
@@ -0,0 +1,25 @@
+# Lists are ordered and mutable(changeble) collections of items
+marks = [54,24,53,56,85]
+
+mixed = [43, "hello", 4.9, False, True]
+
+print(marks)
+print(mixed)
+
+print(marks[0])
+print(marks[1])
+print(marks[2])
+print(marks[3])
+print(marks[4])
+
+print(mixed[0])
+print(mixed[1])
+print(mixed[2])
+print(mixed[3])
+print(mixed[4])
+
+print(type(mixed[0]))
+print(type(mixed[1]))
+print(type(mixed[2]))
+print(type(mixed[3]))
+print(type(mixed[4]))
---
+++ 05_DATA STRUCTURES/01_LISTS & LIST METHODS/03_p2.py
@@ -0,0 +1,9 @@
+# Create a list of numbers from 1 to 10.
+
+# Print the first three numbers using slicing.
+# Print the last three numbers using slicing.
+
+list = [i for i in range(1,11)]
+
+print(list[0:3])
+print(list[-3:])
